8d6d38356bb5f9ae8a7510060ccc19c65ab7746e
Deborah McVey <deborah.mcvey@gmail.com>
2024-05-11 15:54:07 -0400

    this is working. highlights text from paragraph as typed in input field

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HighlightDirective } from './directives/highlight.directive';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    FormsModule,
+    HighlightDirective
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'highlight_example';
+  searchText = '';
 }
+
+// need formsmodule to get searchText and ngModel to work in input
